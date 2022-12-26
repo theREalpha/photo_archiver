153ce16de5676ad7935ae5bec38f8f54948d8a80
theREalpha <78343648+theREalpha@users.noreply.github.com>
2022-12-26 10:46:42 +0530

    :lvl1: lists available albums

---
+++ app.py
@@ -0,0 +1,62 @@
+import pickle
+import os
+from google_auth_oauthlib.flow import Flow, InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+from google.auth.transport.requests import Request
+import pandas as pd
+
+API_NAME = 'photoslibrary'
+API_VERSION = 'v1'
+CLIENT_SECRET_FILE = 'client_secret.json'
+SCOPES = ['https://www.googleapis.com/auth/photoslibrary']
+
+def Create_Service(client_secret_file, api_name, api_version, *scopes):
+	print(client_secret_file, api_name, api_version, scopes, sep='-')
+	CLIENT_SECRET_FILE = client_secret_file
+	API_SERVICE_NAME = api_name
+	API_VERSION = api_version
+	SCOPES = [scope for scope in scopes[0]]
+
+	cred = None
+
+	pickle_file = f'token_{API_SERVICE_NAME}_{API_VERSION}.pickle'
+	# print(pickle_file)
+
+	if os.path.exists(pickle_file):
+		with open(pickle_file, 'rb') as token:
+			cred = pickle.load(token)
+			if not cred or not cred.valid:
+				if cred and cred.expired and cred.refresh_token:
+					cred.refresh(Request())
+			else:
+				flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+				cred = flow.run_local_server()
+
+		with open(pickle_file, 'wb') as token:
+			pickle.dump(cred, token)
+
+	try:
+		service = build(API_SERVICE_NAME, API_VERSION, credentials=cred, static_discovery=False)
+		print(API_SERVICE_NAME, 'service created successfully')
+		return service
+	except Exception as e:
+		print(e)
+		return None
+
+
+service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+##results is a dict
+results = service.albums().list(pageSize=10).execute()
+##albums is a list
+albums= results['albums']
+print(f"No.of Albums: {len(albums)}")
+for album in albums:
+	print()
+	alb_id=album['id']
+	title=album['title']
+	myalb=service.albums().get(albumId=alb_id).execute()
+	for key,val in myalb.items():
+		print(f"{key}: {val}")
+#        break
+kk= pd.DataFrame(albums)
