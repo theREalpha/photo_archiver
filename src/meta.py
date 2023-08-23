3c78c97d60c491b544bbef1d1ce05346b116d975
theREalpha <78343648+theREalpha@users.noreply.github.com>
2023-08-23 23:35:49 +0530

    working metadata writer for jpg,webp,tiff

--- src/meta.py
+++ src/meta.py
@@ -1,6 +1,7 @@
 import piexif
 from datetime import datetime
 from src.objects import MediaItem
+from src.logger import logger
 
 def write_metadata(media: MediaItem, path: str) -> bool:
     '''
@@ -8,7 +9,7 @@ def write_metadata(media: MediaItem, path: str) -> bool:
 
     This function takes a MediaItem object representing media metadata and a file path,
     and writes the relevant metadata to the image file at the specified path.
-
+    Note: works only for jpg, webp, tiff formats currently
     Parameters:
         media (MediaItem): A MediaItem object containing metadata.
         path (str): The path to the image file where metadata will be written.
@@ -16,6 +17,11 @@ def write_metadata(media: MediaItem, path: str) -> bool:
     Returns:
         bool: True if metadata is successfully written, False otherwise.
     '''
+
+    allowed=['jpg','jpeg','webp','tiff']
+    if media.extension not in allowed:
+        logger.error(f"Invalid filetype {media.extension} for writing metadata")
+        return False
 
     meta = media.mediaMetadata
     exif_dict = {"0th": {}, "Exif": {}}
@@ -28,20 +34,19 @@ def write_metadata(media: MediaItem, path: str) -> bool:
     adjusted_creation_time = creation_time + delta
     exif_dict["Exif"][piexif.ExifIFD.DateTimeOriginal] = adjusted_creation_time.strftime("%Y:%m:%d %H:%M:%S")
 
-    if media.is_photo():
-        meta = meta['photo']
-        exif_dict["0th"][piexif.ImageIFD.Make] = meta.get('cameraMake', '').encode('utf-8')
-        exif_dict["0th"][piexif.ImageIFD.Model] = meta.get('cameraModel', '').encode('utf-8')
-        if 'exposureTime' in meta:
-            exif_dict["Exif"][piexif.ExifIFD.ExposureTime] = (int(float(meta['exposureTime'][:-1])*1000000), 1000000)
-        if 'isoEquivalent' in meta:
-            exif_dict["Exif"][piexif.ExifIFD.ISOSpeedRatings] = [int(meta['isoEquivalent'])]
-        if 'apertureFNumber' in meta:
-            exif_dict["Exif"][piexif.ExifIFD.FNumber] = (int(float(meta['apertureFNumber'])*1000), 1000)
-        if 'focalLength' in meta:
-            exif_dict["Exif"][piexif.ExifIFD.FocalLength] = (int(float(meta['focalLength'])*1000), 1000)
-    else:
-        exif_dict["Exif"][piexif.ExifIFD.ExposureTime] = (1, int(meta['video']['fps']))
+    meta = meta['photo']
+    if 'cameraMake' in meta:
+        exif_dict["0th"][piexif.ImageIFD.Make] = meta['cameraMake'].encode('utf-8')
+    if 'cameraModel' in meta:
+        exif_dict["0th"][piexif.ImageIFD.Model] = meta['cameraModel'].encode('utf-8')
+    if 'exposureTime' in meta:
+        exif_dict["Exif"][piexif.ExifIFD.ExposureTime] = (int(float(meta['exposureTime'][:-1])*1000000), 1000000)
+    if 'isoEquivalent' in meta:
+        exif_dict["Exif"][piexif.ExifIFD.ISOSpeedRatings] = [int(meta['isoEquivalent'])]
+    if 'apertureFNumber' in meta:
+        exif_dict["Exif"][piexif.ExifIFD.FNumber] = (int(float(meta['apertureFNumber'])*1000), 1000)
+    if 'focalLength' in meta:
+        exif_dict["Exif"][piexif.ExifIFD.FocalLength] = (int(float(meta['focalLength'])*1000), 1000)
 
     exif_bytes = piexif.dump(exif_dict)
     piexif.insert(exif_bytes, path)
